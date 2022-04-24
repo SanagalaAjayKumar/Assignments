68940498931d6d9cf3034bf214a0538af4f67100
Ajay Kumar Sanagala <a.sanagala@zensar.com>
2022-04-24 21:43:08 +0530

    C#Assignment2

---
+++ C#/Assignment2/Assignment2/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+              // first avreage
+                int[] marks = { 10, 30, 50, 60, 80 };
+                int n = 0;
+                foreach (int i in marks) 
+                {
+                    n += i;
+                }
+                Console.WriteLine("average marks :{0}", n / marks.Length);
+                Console.WriteLine("minimun marks :{0}", marks.Min());
+                Console.WriteLine("maximum marks :{0}", marks.Max());
+
+               //2 maximum and minimum
+
+              int[] marks1 = new int[4];
+               int n1 = 0;
+               Console.WriteLine("Enter Marks : ");
+               for (int i = 0; i < marks1.Length; i++)
+               {
+                   marks1[i] = Convert.ToInt32(Console.ReadLine());
+               }
+               foreach (int i in marks1)
+               {
+                   n1 += i;
+
+               }
+               Console.WriteLine("Total marks is {0}", n1);
+               Console.WriteLine("Average Marks {0}", n1 / 4);
+               Console.WriteLine("Maximum marks is {0}", marks1.Max());
+               Console.WriteLine("Minimum marks is {0}", marks1.Min());
+               Array.Sort(marks1);
+
+               Console.WriteLine("Ascending order ");
+               foreach (int i in marks1)
+               {
+                   Console.Write(i + " ");
+               }
+               Array.Reverse(marks1);
+               Console.WriteLine(" ");
+               Console.WriteLine("Descending order ");
+               foreach (int i in marks1)
+               {
+                   Console.Write(i + " ");
+               }
+               Console.WriteLine(" ");
+
+               //3 A. length of string
+
+               Console.WriteLine("Enter your name:");
+               string name = Console.ReadLine();
+               Console.WriteLine("Length of the string :{0}", name.Length);
+
+
+                // 3.b reverse string
+
+                Console.WriteLine("Enter your name");
+
+                string str = Console.ReadLine();
+                string str2= string.Empty;
+                for (int i =  str.Length - 1; i >= 0; i--)  
+                {
+                    str2 += str[i];
+                }
+
+                Console.WriteLine(str2);
+
+                //3.c same or not
+
+                string str1;
+                string str3;
+                Console.WriteLine("Enter num:");
+                Console.WriteLine("Enter num:");
+                str1 = Console.ReadLine();
+                str3 = Console.ReadLine();
+
+                if (str1 == str3)
+                {
+                    Console.WriteLine("Two Number are equal");
+                }
+                else if(str1 != str2)
+                {
+                    Console.WriteLine(" two Number are not equal");
+                }
+
+               // 4. plaindrom or not
+
+            Console.WriteLine("Enter a String");
+            string str5, revs = "";
+            str5 = Console.ReadLine();
+
+            for (int i = str5.Length - 1; i >= 0; i--)
+            {
+                revs += str5[i].ToString();
+            }
+            if (revs == str5)
+            {
+                Console.WriteLine("String is a plaindrom ", str, revs);
+            }
+            
+            {
+                Console.WriteLine("string is not a palindrome");
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ C#/Assignment3/Assignment3/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    // 1.student details
+    internal class Student
+    {
+        int rollno;
+        String name;
+        String branch;
+        String sem;
+        String clas;
+
+        static public void displayResult()
+        {
+            int[] marks = new int[5];
+            marks[0] = 10;
+            marks[1] = 30;
+            marks[2] = 50;
+            marks[3] = 80;
+            marks[4] = 90;
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine(" marks of " + i + " subject : " + marks[i]);
+            }
+
+            double average = marks.Average();
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                if (marks[i] < 35)
+                {
+                    Console.WriteLine("FAIL");
+                }
+                else if (marks[i] > 35 && average < 50)
+                {
+                    Console.WriteLine("FAIL");
+                }
+                else if (average > 50)
+                {
+                    Console.WriteLine("PASS");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID");
+                }
+            }
+        }
+        public Student(int rollno, String name, String branch, String sem, String clas)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.branch = branch;
+            this.sem = sem;
+            this.clas = clas;
+            Console.WriteLine($"\nRollno is: {rollno}, Name is {name},Student class is {clas}, Sem is {sem},Branch is : {branch}");
+        }
+        static public void displayData()
+        {
+            Console.WriteLine();
+        }
+    }
+
+    // 2.car details
+
+    internal class Car
+    {
+        int car_no;
+        string car_name;
+        string car_type;
+        readonly double cost = 500000;
+        public Car(int car_no, string car_name, string car_type, double cost)
+        {
+            this.car_no = car_no;
+            this.car_name = car_name;
+            this.car_type = car_type;
+            this.cost = cost;
+            Console.WriteLine($"\nthe car_no  {car_no},car_name is {car_name},car_type {car_type},price is {cost} ");
+        }
+        public void showDetails()
+        {
+            Console.WriteLine("\nbefore changing");
+            Console.WriteLine($"the car_no  {car_no},car_name is {car_name},car_type is {car_type},price is {cost} ");
+        }
+        public Car(double car_cost)
+        {
+            Console.WriteLine("\nafter changing");
+            car_cost = cost;
+            Console.WriteLine($"the car_no  {car_no},car_name is {car_name},car_type is {car_type},price is {cost} ");
+
+        }
+    }
+
+    //3.bank loan
+    internal class bank
+    {
+        double interestRate;
+        public void Bankloan(double loanamt)
+        {
+            Console.WriteLine("\nPlease enter rate of interest");
+            interestRate = Convert.ToDouble(Console.ReadLine());
+            double interest = (loanamt * interestRate) / 100;
+            Console.WriteLine("\nRate of interest for " + interestRate + " percentage %  is : "+ interest);
+        }
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Student.displayResult();
+            Student student = new Student(204, "Ajay", "Electrical", "4-2", "Btech");
+            Student.displayData();
+
+            
+            Car car = new Car(007, "Audi", "sports", 7000000);
+            car.showDetails();
+            Car car2 = new Car(40000000);
+
+         
+            bank bank = new bank();
+            Console.WriteLine("\n enter the loan amount: ");
+            bank.Bankloan(Convert.ToInt32(Console.ReadLine()));
+
+            Console.ReadLine();
+
+        }
+    }
+
+
+}
